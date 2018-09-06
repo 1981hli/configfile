91a6020790a7dc177e06b7a046142d8d223565fe
lihuan <1981hli@gmail.com>
2018-09-06 09:42:06 +0800

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 ln -s ~/.bashrc       ~/src/github/1981hli/ConfigFile/bashrc
 ln -s ~/.tmux.conf    ~/src/github/1981hli/ConfigFile/tmux.conf
 ln -s ~/.vim          ~/src/github/1981hli/ConfigFile/vim
