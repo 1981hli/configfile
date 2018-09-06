342a46237811a58c86d88ec6021000268b5c5489
lihuan <1981hli@gmail.com>
2018-09-06 09:36:31 +0800

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ln -s ~/.bashrc       ~/src/github/1981hli/configfile/bashrc
-ln -s ~/.tmux.conf    ~/src/github/1981hli/configfile/tmux.conf
-ln -s ~/.vim          ~/src/github/1981hli/configfile/vim
-ln -s ~/.vimrc        ~/src/github/1981hli/configfile/vimrc
-ln -s ~/.zshrc        ~/src/github/1981hli/configfile/zshrc
+ln -s ~/.bashrc       ~/src/github/1981hli/ConfigFile/bashrc
+ln -s ~/.tmux.conf    ~/src/github/1981hli/ConfigFile/tmux.conf
+ln -s ~/.vim          ~/src/github/1981hli/ConfigFile/vim
+ln -s ~/.vimrc        ~/src/github/1981hli/ConfigFile/vimrc
+ln -s ~/.zshrc        ~/src/github/1981hli/ConfigFile/zshrc
